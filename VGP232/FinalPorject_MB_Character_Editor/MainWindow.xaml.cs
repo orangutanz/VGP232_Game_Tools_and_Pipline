f7c44af97abaa690ee15e3f35ee4470065267927
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2020-11-19 13:43:15 -0800

    FinalProject documents

---
+++ VGP232/FinalPorject_MB_Character_Editor/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalPorject_MB_Character_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuSaveAs(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuExit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuSave(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuOpen(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuNew(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
