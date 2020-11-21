47fb70adbb1b1632d2dd0aaa639689cb7fdc9475
Lawrence Cheung <lcheunglci@gmail.com>
2020-11-21 15:07:09 -0800

    Marked Assignment 3

--- VGP232/Assignment3/MainWindow.xaml.cs
+++ VGP232/Assignment3/MainWindow.xaml.cs
@@ -20,6 +20,11 @@ using TextureAtlasLib;
 // Assignment 3
 // NAME: Yuhan Ma
 // STUDENT NUMBER: 1930014
+// MARKS: 68/100 Good effort. The save and load xml should be implementation in the TextureAtlasLib, and it wasn't implemented correctly. Browse button is suppose to set 2 textboxes the OutputFile and the OutputDirectory. It suppose to serialize the Spritesheet class from the SaveDialog.FileName not the Spritesheet.OuptutFile. Generate doesn't have a try catch to show the messagebox when an exception is thrown and when it succeeds it should show a messsage box to take the user to the folder. The OutputDirectory is not data binded properly so an exception is thrown from validation when you Generate.
+// Does it compile? Yes
+// Does it produce the correct results? No
+// Is using databinding? Yes, but the outputDirectory wasn't binded.
+// Does the UI look like the mock up? Yes
 
 namespace Assignment3
 {
@@ -29,12 +34,16 @@ namespace Assignment3
     public partial class MainWindow : Window
     {
         public Spritesheet MySpriteSheets { get; set; }
+        // LC: there should be another property here for the file to save or load.
 
         public MainWindow()
         {
             InitializeComponent();
+
+            // LC: should be set after the spritesheet is initialized.
             DataContext = MySpriteSheets;
             MySpriteSheets = new Spritesheet();
+            // LC: add a default constructor with the list initialization
             MySpriteSheets.InputPaths = new List<String>();
             lbImages.ItemsSource = MySpriteSheets.InputPaths;
             
@@ -69,6 +78,8 @@ namespace Assignment3
 
         private void GeneratePressed(object sender, RoutedEventArgs e)
         {
+            // LC: wrap this in try catch because it will throw an exception if the properties are not set properly
+            // also when this succeeds it should show a message box asking if the user wants to go to the output folder.
             MySpriteSheets.Generate(true);
         }
 
@@ -77,6 +88,8 @@ namespace Assignment3
             SaveFileDialog saveFile = new SaveFileDialog();
             if (saveFile.ShowDialog() == true)
             {
+                // LC: System.IO.Path.GetFileName and System.IO.Path.GetDirectoryName
+                // LC: this should be databinded so you just need to update MySpriteSheets properties with the outputFile and outputDirectory
                 tbOutputDir.Text = saveFile.FileName;
                 MySpriteSheets.OutputFile = saveFile.FileName;
             }
@@ -96,17 +109,21 @@ namespace Assignment3
             OpenFileDialog openFile = new OpenFileDialog();
             if (openFile.ShowDialog() == true)
             {
+                // LC: this should be added to the textureatlaslib spritesheet class in Load function.
                 XmlSerializer xml = new XmlSerializer(typeof(List<String>));
                 try
                 {
                     using (StreamReader reader = new StreamReader(openFile.FileName))
                     {
+                        // LC: we are not deserializing the list of images use, but the entire Spritesheet classs.
                         MySpriteSheets.InputPaths.Clear();
                         MySpriteSheets.InputPaths = (List<String>)xml.Deserialize(reader);
                     }
                 }
                 catch (Exception)
                 {
+                    // throw an exception of why it wasn't able to be saved.
+                    // LC: show an error saying it wasn't able to open the file.
                 }
 
             }
@@ -114,10 +131,11 @@ namespace Assignment3
 
         private void MenuSave(object sender, RoutedEventArgs e)
         {
-
+            // LC: Add this in Spritesheet class in a method called Save
             XmlSerializer xml = new XmlSerializer(typeof(Spritesheet));
             try
             {
+                // LC: 
                 using (StreamWriter writer = new StreamWriter(MySpriteSheets.OutputFile))
                 {
                     xml.Serialize(writer, MySpriteSheets);                     
@@ -125,6 +143,8 @@ namespace Assignment3
             }
             catch (Exception)
             {
+                // throw an exception of why it wasn't able to be saved.
+                // and catch this exception and put the message into a MessageBox
             }
         }
     
@@ -134,15 +154,17 @@ namespace Assignment3
             SaveFileDialog saveFile = new SaveFileDialog();
             if (saveFile.ShowDialog() == true)
             {
+                // LC: you're not setting any of the properties, but the label that says the file name.
                 tbOutputDir.Text = saveFile.FileName;
                 MySpriteSheets.OutputFile = saveFile.FileName;
             }
+            // LC: should be inside the body of the if statement.
             MenuSave(sender, e);
         }
 
         private void MenuExit(object sender, RoutedEventArgs e)
         {
-
+            // LC: missing implementation
         }
     }
 }
